587d4fc07799dcc1d88035498a9370d5bb52b1f1
James Harrison <james@fasttortoise.co.uk>
2021-11-05 16:24:25 +0000

    Support for MinecraftCapes

--- src/main/java/dev/tr7zw/waveycapes/support/SupportManager.java
+++ src/main/java/dev/tr7zw/waveycapes/support/SupportManager.java
@@ -8,17 +8,33 @@ public class SupportManager {
     public static Set<ModSupport> mods = new HashSet<>();
     
     static {
-        try {
-            Class.forName("de.kxmischesdomi.morebannerfeatures.MoreBannerFeatures");
+        if(doesClassExist("de.kxmischesdomi.morebannerfeatures.MoreBannerFeatures")) {
             mods.add(new MoreBannerFeaturesSupport());
             System.out.println("Wavey Capes loaded MoreBannerFeatures support!");
-        }catch(Exception ex) {
-            // not loaded
+        }
+
+        if(doesClassExist("net.minecraftcapes.MinecraftCapes")) {
+            mods.add(new MinecraftCapesSupport());
+            System.out.println("Wavey Capes loaded MinecraftCapes support!");
         }
     }
     
     public static Set<ModSupport> getSupportedMods(){
         return mods;
     }
-    
+
+    /**
+     * Checks if a class exists or not
+     * @param name
+     * @return
+     */
+    private static boolean doesClassExist(String name) {
+        try {
+            if(Class.forName(name) != null) {
+                return true;
+            }
+        } catch (ClassNotFoundException e) {}
+        return false;
+    }
+
 }
