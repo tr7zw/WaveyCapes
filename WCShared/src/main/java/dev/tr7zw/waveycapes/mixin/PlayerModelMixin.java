6161bfdade7d5b95c8aff2cfe1c004aee8af4b82
tr7zw <tr7zw@live.de>
2021-11-05 20:15:55 +0100

    Refactor to increase compatebility with other mods

--- WCShared/src/main/java/dev/tr7zw/waveycapes/mixin/PlayerModelMixin.java
+++ WCShared/src/main/java/dev/tr7zw/waveycapes/mixin/PlayerModelMixin.java
@@ -1,56 +1,23 @@
 package dev.tr7zw.waveycapes.mixin;
 
-import java.util.NoSuchElementException;
-
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 
-import dev.tr7zw.waveycapes.accessor.PlayerEntityModelAccessor;
 import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.client.model.PlayerModel;
 import net.minecraft.client.model.geom.ModelPart;
-import net.minecraft.client.model.geom.PartPose;
-import net.minecraft.client.model.geom.builders.CubeDeformation;
-import net.minecraft.client.model.geom.builders.CubeListBuilder;
-import net.minecraft.client.model.geom.builders.MeshDefinition;
-import net.minecraft.client.model.geom.builders.PartDefinition;
 import net.minecraft.world.entity.LivingEntity;
 
-@Mixin(PlayerModel.class)
-public class PlayerModelMixin<T extends LivingEntity> extends HumanoidModel<T> implements PlayerEntityModelAccessor {
+@Mixin(value =  PlayerModel.class)
+public class PlayerModelMixin<T extends LivingEntity> extends HumanoidModel<T> {
 
     public PlayerModelMixin(ModelPart modelPart) {
         super(modelPart);
-    }
-
-    private ModelPart[] customCape = new ModelPart[16];
-
-    @Inject(method = "<init>*", at = @At("RETURN"))
-    public void onCreate(ModelPart modelPart, boolean bl, CallbackInfo info) {
-        try {
-            for (int i = 0; i < 16; i++) {
-                this.customCape[i] = modelPart.getChild("customCape_" + i);
-            }
-        }catch(NoSuchElementException ex) {
-            // Do nothing. The "MinecraftCapes Mod" somehow causes Piglins to call this code?!?
-        }
-    }
-
-    @Inject(method = "createMesh", at = @At("RETURN"))
-    private static void createMesh(CubeDeformation cubeDeformation, boolean bl,
-            CallbackInfoReturnable<MeshDefinition> info) {
-        PartDefinition partDefinition = info.getReturnValue().getRoot();
-        for (int i = 0; i < 16; i++)
-            partDefinition.addOrReplaceChild("customCape_" + i,
-                    CubeListBuilder.create().texOffs(0, i)
-                            .addBox(-5.0F, i, -1.0F, 10.0F, 1.0F, 1.0F, cubeDeformation, 1.0F, 0.5F),
-                    PartPose.offset(0.0F, 0.0F, 0.0F));
     }
 
     @Inject(method = "renderCloak", at = @At("HEAD"), cancellable = true)
@@ -58,9 +25,5 @@ public class PlayerModelMixin<T extends LivingEntity> extends HumanoidModel<T> i
         info.cancel();
     }
 
-    @Override
-    public ModelPart[] getCustomCapeParts() {
-        return customCape;
-    }
 
 }
