2622bd54e623dd8a59b105b2cbf9e288ca1cb0b2
tr7zw <tr7zw@live.de>
2022-03-14 21:47:43 +0100

    Auto disable the smooth cape when not compatible

--- WCShared/src/main/java/dev/tr7zw/waveycapes/config/Config.java
+++ WCShared/src/main/java/dev/tr7zw/waveycapes/config/Config.java
@@ -10,7 +10,7 @@ public class Config {
     public WindMode windMode = WindMode.NONE;
     public CapeStyle capeStyle = CapeStyle.SMOOTH;
     public CapeMovement capeMovement = CapeMovement.BASIC_SIMULATION;
-    public int capeParts = 16;
+    //public int capeParts = 16;
     public int gravity = -25;
     public int maxBend = 5;
 }
