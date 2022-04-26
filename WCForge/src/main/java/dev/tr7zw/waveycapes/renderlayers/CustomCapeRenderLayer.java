4f1015f1a9419b23454584cd71375d2e04fd5e9f
tr7zw <tr7zw@live.de>
2022-04-26 15:29:13 +0200

    Testing to backport the 1.17 smooth render logic

--- WCForge/src/main/java/dev/tr7zw/waveycapes/renderlayers/CustomCapeRenderLayer.java
+++ WCForge/src/main/java/dev/tr7zw/waveycapes/renderlayers/CustomCapeRenderLayer.java
@@ -5,11 +5,17 @@ import dev.tr7zw.waveycapes.CapeMovement;
 import dev.tr7zw.waveycapes.WaveyCapesBase;
 import dev.tr7zw.waveycapes.WindMode;
 import dev.tr7zw.waveycapes.sim.StickSimulation;
+import dev.tr7zw.waveycapes.util.Matrix4f;
 import dev.tr7zw.waveycapes.util.Mth;
+import dev.tr7zw.waveycapes.util.PoseStack;
+import dev.tr7zw.waveycapes.util.Vector3f;
+import dev.tr7zw.waveycapes.util.Vector4f;
 import net.minecraft.client.entity.AbstractClientPlayer;
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.WorldRenderer;
 import net.minecraft.client.renderer.entity.RenderPlayer;
 import net.minecraft.client.renderer.entity.layers.LayerRenderer;
 import net.minecraft.entity.player.EnumPlayerModelParts;
@@ -54,14 +60,15 @@ public class CustomCapeRenderLayer implements LayerRenderer<AbstractClientPlayer
             CapeHolder holder = (CapeHolder) abstractClientPlayer;
             holder.updateSimulation(abstractClientPlayer, partCount);
         }
-        
+        this.playerRenderer.bindTexture(abstractClientPlayer.getLocationCape());
+        renderSmoothCape(abstractClientPlayer, deltaTick);
+        if(true)return;
 //        if (WaveyCapesBase.config.capeStyle == CapeStyle.SMOOTH && renderer.vanillaUvValues()) {
 //            renderSmoothCape(poseStack, multiBufferSource, renderer, abstractClientPlayer, delta, i);
 //        } else {
         ModelRenderer[] parts = customCape;
         for (int part = 0; part < partCount; part++) {
             ModelRenderer model = parts[part];
-            this.playerRenderer.bindTexture(abstractClientPlayer.getLocationCape());
             GlStateManager.pushMatrix();
             modifyPoseStack(abstractClientPlayer, deltaTick, part);
             model.render(0.0625F);
@@ -71,29 +78,33 @@ public class CustomCapeRenderLayer implements LayerRenderer<AbstractClientPlayer
 //        }
     }
     
-//    private void renderSmoothCape(PoseStack poseStack, MultiBufferSource multiBufferSource, CapeRenderer capeRenderer, AbstractClientPlayer abstractClientPlayer, float delta, int light) {
-//        VertexConsumer bufferBuilder = capeRenderer.getVertexConsumer(multiBufferSource, abstractClientPlayer);
+    private void renderSmoothCape(AbstractClientPlayer abstractClientPlayer, float delta) {
+        WorldRenderer worldrenderer = Tessellator.getInstance().getWorldRenderer();
+        GlStateManager.enableBlend();
+        PoseStack poseStack = new PoseStack();
+        GlStateManager.getFloat(partCount, null);
+        
 //        RenderSystem.enableBlend();
 //        RenderSystem.defaultBlendFunc();
-//
-//        Matrix4f oldPositionMatrix = null;
-//        for (int part = 0; part < partCount; part++) {
-//            modifyPoseStack(poseStack, abstractClientPlayer, delta, part);
-//
-//            if (oldPositionMatrix == null) {
-//                oldPositionMatrix = poseStack.last().pose();
-//            }
-//
-//            if (part == 0) {
-//                addTopVertex(bufferBuilder, poseStack.last().pose(), oldPositionMatrix,
-//                        0.3F,
-//                        0,
-//                        0F,
-//                        -0.3F,
-//                        0,
-//                        -0.06F, part, light);
-//            }
-//
+
+        Matrix4f oldPositionMatrix = null;
+        for (int part = 0; part < partCount; part++) {
+            modifyPoseStack(poseStack, abstractClientPlayer, delta, part);
+
+            if (oldPositionMatrix == null) {
+                oldPositionMatrix = poseStack.last().pose();
+            }
+
+            if (part == 0) {
+                addTopVertex(worldrenderer, poseStack.last().pose(), oldPositionMatrix,
+                        0.3F,
+                        0,
+                        0F,
+                        -0.3F,
+                        0,
+                        -0.06F, part);
+            }
+
 //            if (part == partCount - 1) {
 //                addBottomVertex(bufferBuilder, poseStack.last().pose(), poseStack.last().pose(),
 //                        0.3F,
@@ -112,13 +123,13 @@ public class CustomCapeRenderLayer implements LayerRenderer<AbstractClientPlayer
 //                    part * (0.96F / partCount),
 //                    -0.06F, part, light);
 //
-//            addRightVertex(bufferBuilder, poseStack.last().pose(), oldPositionMatrix,
-//                    0.3F,
-//                    (part + 1) * (0.96F / partCount),
-//                    0F,
-//                    0.3F,
-//                    part * (0.96F / partCount),
-//                    -0.06F, part, light);
+            addRightVertex(worldrenderer, poseStack.last().pose(), oldPositionMatrix,
+                    0.3F,
+                    (part + 1) * (0.96F / partCount),
+                    0F,
+                    0.3F,
+                    part * (0.96F / partCount),
+                    -0.06F, part);
 //
 //            addBackVertex(bufferBuilder, poseStack.last().pose(), oldPositionMatrix,
 //                    0.3F,
@@ -135,19 +146,73 @@ public class CustomCapeRenderLayer implements LayerRenderer<AbstractClientPlayer
 //                    -0.3F,
 //                    part * (0.96F / partCount),
 //                    0F, part, light);
-//
-//            oldPositionMatrix = poseStack.last().pose();
-//            poseStack.popPose();
-//        }
-//
-//    }
 
+            oldPositionMatrix = poseStack.last().pose();
+            poseStack.popPose();
+        }
+
+    }
+
+    private void modifyPoseStack(PoseStack poseStack, AbstractClientPlayer abstractClientPlayer, float h, int part) {
+        if(WaveyCapesBase.config.capeMovement == CapeMovement.BASIC_SIMULATION) {
+            modifyPoseStackSimulation(poseStack, abstractClientPlayer, h, part);
+            return;
+        }
+        modifyPoseStackVanilla(abstractClientPlayer, h, part);
+    }
+    
     private void modifyPoseStack(AbstractClientPlayer abstractClientPlayer, float h, int part) {
         if(WaveyCapesBase.config.capeMovement == CapeMovement.BASIC_SIMULATION) {
             modifyPoseStackSimulation(abstractClientPlayer, h, part);
             return;
         }
         modifyPoseStackVanilla(abstractClientPlayer, h, part);
+    }
+    
+    private void modifyPoseStackSimulation(PoseStack poseStack, AbstractClientPlayer abstractClientPlayer, float delta, int part) {
+        StickSimulation simulation = ((CapeHolder)abstractClientPlayer).getSimulation();
+        poseStack.pushPose();
+        poseStack.translate(0.0D, 0.0D, 0.125D);
+        
+        float z = simulation.points.get(part).getLerpX(delta) - simulation.points.get(0).getLerpX(delta);
+        if(z > 0) {
+            z = 0;
+        }
+        float y = simulation.points.get(0).getLerpY(delta) - part - simulation.points.get(part).getLerpY(delta);
+        
+//        float sidewaysRotationOffset = (float) (d * p - m * o) * 100.0F;
+//        sidewaysRotationOffset = Mth.clamp(sidewaysRotationOffset, -20.0F, 20.0F);
+        float sidewaysRotationOffset = 0;
+        float partRotation = (float) -Math.atan2(y, z);
+        partRotation = Math.max(partRotation, 0);
+        if(partRotation != 0)
+            partRotation = (float) (Math.PI-partRotation);
+        partRotation *= 57.2958;
+        partRotation *= 2;
+        
+        float height = 0;
+        if (abstractClientPlayer.isSneaking()) {
+            height += 25.0F;
+            poseStack.translate(0, 0.15F, 0);
+        }
+
+        float naturalWindSwing = getNatrualWindSwing(part);
+
+        
+        // vanilla rotating and wind
+        poseStack.mulPose(Vector3f.XP.rotationDegrees(6.0F + height + naturalWindSwing));
+        poseStack.mulPose(Vector3f.ZP.rotationDegrees(sidewaysRotationOffset / 2.0F));
+        poseStack.mulPose(Vector3f.YP.rotationDegrees(180.0F - sidewaysRotationOffset / 2.0F));
+        poseStack.translate(0, y/partCount, z/partCount); // movement from the simulation
+        //offsetting so the rotation is on the cape part
+        //float offset = (float) (part * (16 / partCount))/16; // to fold the entire cape into one position for debugging
+        poseStack.translate(0, /*-offset*/ + (0.48/16) , - (0.48/16)); // (0.48/16)
+        poseStack.translate(0, part * 1f/partCount, part * (0)/partCount);
+        poseStack.mulPose(Vector3f.XP.rotationDegrees(-partRotation)); // apply actual rotation
+        // undoing the rotation
+        poseStack.translate(0, -part * 1f/partCount, -part * (0)/partCount);
+        poseStack.translate(0, -(0.48/16), (0.48/16));
+        
     }
     
     private void modifyPoseStackSimulation(AbstractClientPlayer abstractClientPlayer, float delta, int part) {
@@ -343,36 +408,43 @@ public class CustomCapeRenderLayer implements LayerRenderer<AbstractClientPlayer
 //        bufferBuilder.vertex(oldMatrix, x2, y2, z1).color(1f, 1f, 1f, 1f).uv(maxU, minV).overlayCoords(OverlayTexture.NO_OVERLAY).uv2(light).normal(1, 0, 0).endVertex();
 //    }
 //
-//    private static void addRightVertex(VertexConsumer bufferBuilder, Matrix4f matrix, Matrix4f oldMatrix, float x1, float y1, float z1, float x2, float y2, float z2, int part, int light) {
-//        float i;
-//        if (x1 < x2) {
-//            i = x1;
-//            x1 = x2;
-//            x2 = i;
-//        }
-//
-//        if (y1 < y2) {
-//            i = y1;
-//            y1 = y2;
-//            y2 = i;
-//        }
-//
-//        float minU = .171875F;
-//        float maxU = .1875F;
-//
-//        float minV = .03125F;
-//        float maxV = .53125F;
-//
-//        float deltaV = maxV - minV;
-//        float vPerPart = deltaV / partCount;
-//        maxV = minV + (vPerPart * (part + 1));
-//        minV = minV + (vPerPart * part);
-//
+    private static void addRightVertex(WorldRenderer worldrenderer, Matrix4f matrix, Matrix4f oldMatrix, float x1, float y1, float z1, float x2, float y2, float z2, int part) {
+        float i;
+        if (x1 < x2) {
+            i = x1;
+            x1 = x2;
+            x2 = i;
+        }
+
+        if (y1 < y2) {
+            i = y1;
+            y1 = y2;
+            y2 = i;
+        }
+
+        float minU = .171875F;
+        float maxU = .1875F;
+
+        float minV = .03125F;
+        float maxV = .53125F;
+
+        float deltaV = maxV - minV;
+        float vPerPart = deltaV / partCount;
+        maxV = minV + (vPerPart * (part + 1));
+        minV = minV + (vPerPart * part);
+
+        //matrix
+        vertex(worldrenderer, matrix, x2, y1, z2).tex(minU, maxV).normal(1, 0, 0).endVertex();
+        vertex(worldrenderer, matrix, x2, y1, z1).tex(maxU, maxV).normal(1, 0, 0).endVertex();
+        //oldMatrix
+        vertex(worldrenderer, oldMatrix, x2, y2, z1).tex(maxU, minV).normal(1, 0, 0).endVertex();
+        vertex(worldrenderer, oldMatrix, x2, y2, z2).tex(minU, minV).normal(1, 0, 0).endVertex();
+        
 //        bufferBuilder.vertex(matrix, x2, y1, z2).color(1f, 1f, 1f, 1f).uv(minU, maxV).overlayCoords(OverlayTexture.NO_OVERLAY).uv2(light).normal(1, 0, 0).endVertex();
 //        bufferBuilder.vertex(matrix, x2, y1, z1).color(1f, 1f, 1f, 1f).uv(maxU, maxV).overlayCoords(OverlayTexture.NO_OVERLAY).uv2(light).normal(1, 0, 0).endVertex();
 //        bufferBuilder.vertex(oldMatrix, x2, y2, z1).color(1f, 1f, 1f, 1f).uv(maxU, minV).overlayCoords(OverlayTexture.NO_OVERLAY).uv2(light).normal(1, 0, 0).endVertex();
 //        bufferBuilder.vertex(oldMatrix, x2, y2, z2).color(1f, 1f, 1f, 1f).uv(minU, minV).overlayCoords(OverlayTexture.NO_OVERLAY).uv2(light).normal(1, 0, 0).endVertex();
-//    }
+    }
 //
 //    private static void addBottomVertex(VertexConsumer bufferBuilder, Matrix4f matrix, Matrix4f oldMatrix, float x1, float y1, float z1, float x2, float y2, float z2, int part, int light) {
 //        float i;
@@ -404,37 +476,50 @@ public class CustomCapeRenderLayer implements LayerRenderer<AbstractClientPlayer
 //        bufferBuilder.vertex(matrix, x2, y1, z1).color(1f, 1f, 1f, 1f).uv(minU, maxV).overlayCoords(OverlayTexture.NO_OVERLAY).uv2(light).normal(1, 0, 0).endVertex();
 //        bufferBuilder.vertex(matrix, x1, y1, z1).color(1f, 1f, 1f, 1f).uv(maxU, maxV).overlayCoords(OverlayTexture.NO_OVERLAY).uv2(light).normal(1, 0, 0).endVertex();
 //    }
-//
-//    private static void addTopVertex(VertexConsumer bufferBuilder, Matrix4f matrix, Matrix4f oldMatrix, float x1, float y1, float z1, float x2, float y2, float z2, int part, int light) {
-//        float i;
-//        if (x1 < x2) {
-//            i = x1;
-//            x1 = x2;
-//            x2 = i;
-//        }
-//
-//        if (y1 < y2) {
-//            i = y1;
-//            y1 = y2;
-//            y2 = i;
-//        }
-//
-//        float minU = .015625F;
-//        float maxU = .171875F;
-//
-//        float minV = 0;
-//        float maxV = .03125F;
-//
-//        float deltaV = maxV - minV;
-//        float vPerPart = deltaV / partCount;
-//        maxV = minV + (vPerPart * (part + 1));
-//        minV = minV + (vPerPart * part);
-//
+
+    private static WorldRenderer vertex(WorldRenderer worldrenderer, Matrix4f matrix4f, float f, float g, float h) {
+        Vector4f vector4f = new Vector4f(f, g, h, 1.0F);
+        vector4f.transform(matrix4f);
+        worldrenderer.pos(vector4f.x(), vector4f.y(), vector4f.z());
+        return worldrenderer;
+    }
+    
+    private static void addTopVertex(WorldRenderer worldrenderer, Matrix4f matrix, Matrix4f oldMatrix, float x1, float y1, float z1, float x2, float y2, float z2, int part) {
+        float i;
+        if (x1 < x2) {
+            i = x1;
+            x1 = x2;
+            x2 = i;
+        }
+
+        if (y1 < y2) {
+            i = y1;
+            y1 = y2;
+            y2 = i;
+        }
+
+        float minU = .015625F;
+        float maxU = .171875F;
+
+        float minV = 0;
+        float maxV = .03125F;
+
+        float deltaV = maxV - minV;
+        float vPerPart = deltaV / partCount;
+        maxV = minV + (vPerPart * (part + 1));
+        minV = minV + (vPerPart * part);
+
+        //oldMatrix
+        vertex(worldrenderer, oldMatrix, x1, y2, z1).tex(maxU, maxV).normal(0, 1, 0).endVertex();
+        vertex(worldrenderer, oldMatrix, x2, y2, z1).tex(minU, maxV).normal(0, 1, 0).endVertex();
+        //newMatrix
+        vertex(worldrenderer, matrix, x2, y1, z2).tex(minU, minV).normal(0, 1, 0).endVertex();
+        vertex(worldrenderer, matrix, x1, y1, z2).tex(maxU, minV).normal(0, 1, 0).endVertex();
 //        bufferBuilder.vertex(oldMatrix, x1, y2, z1).color(1f, 1f, 1f, 1f).uv(maxU, maxV).overlayCoords(OverlayTexture.NO_OVERLAY).uv2(light).normal(0, 1, 0).endVertex();
 //        bufferBuilder.vertex(oldMatrix, x2, y2, z1).color(1f, 1f, 1f, 1f).uv(minU, maxV).overlayCoords(OverlayTexture.NO_OVERLAY).uv2(light).normal(0, 1, 0).endVertex();
 //        bufferBuilder.vertex(matrix, x2, y1, z2).color(1f, 1f, 1f, 1f).uv(minU, minV).overlayCoords(OverlayTexture.NO_OVERLAY).uv2(light).normal(0, 1, 0).endVertex();
 //        bufferBuilder.vertex(matrix, x1, y1, z2).color(1f, 1f, 1f, 1f).uv(maxU, minV).overlayCoords(OverlayTexture.NO_OVERLAY).uv2(light).normal(0, 1, 0).endVertex();
-//    }
+    }
 
 //    private static VanillaCapeRenderer vanillaCape = new VanillaCapeRenderer();
 //    
--- WCForge/src/main/java/dev/tr7zw/waveycapes/util/Mth.java
+++ WCForge/src/main/java/dev/tr7zw/waveycapes/util/Mth.java
@@ -10,4 +10,22 @@ public class Mth {
         return e + d * (f - e);
     }
     
+    public static float fastInvSqrt(float f) {
+        float g = 0.5F * f;
+        int i = Float.floatToIntBits(f);
+        i = 1597463007 - (i >> 1);
+        f = Float.intBitsToFloat(i);
+        f *= 1.5F - g * f * f;
+        return f;
+    }
+    
+    public static float fastInvCubeRoot(float f) {
+        int i = Float.floatToIntBits(f);
+        i = 1419967116 - i / 3;
+        float g = Float.intBitsToFloat(i);
+        g = 0.6666667F * g + 1.0F / 3.0F * g * g * f;
+        g = 0.6666667F * g + 1.0F / 3.0F * g * g * f;
+        return g;
+    }
+    
 }
