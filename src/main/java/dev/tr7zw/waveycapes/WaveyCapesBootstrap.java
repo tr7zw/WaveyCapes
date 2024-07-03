92cc9b110a6dcc6000d7b8026762268823d41bc4
tr7zw <tr7zw@live.de>
2024-07-03 20:18:58 +0200

    Fix neoforge 1.21

--- src/main/java/dev/tr7zw/waveycapes/WaveyCapesBootstrap.java
+++ src/main/java/dev/tr7zw/waveycapes/WaveyCapesBootstrap.java
@@ -19,17 +19,17 @@
 //$$package dev.tr7zw.waveycapes;
 //$$
 //$$import net.neoforged.api.distmarker.Dist;
-//$$import net.neoforged.fml.DistExecutor;
+//$$import net.neoforged.fml.loading.FMLEnvironment;
 //$$import net.neoforged.fml.common.Mod;
 //$$
 //$$@Mod("waveycapes")
 //$$public class WaveyCapesBootstrap {
 //$$
-//$$	public WaveyCapesBootstrap() {
-//$$		DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> { 
-//$$         new WaveyCapesMod().init();
-//$$        });
-//$$	}
-//$$	
+//$$    public WaveyCapesBootstrap() {
+//$$            if(FMLEnvironment.dist == Dist.CLIENT) {
+//$$                new WaveyCapesMod().init();
+//$$            }
+//$$    }
+//$$
 //$$}
 //#endif
